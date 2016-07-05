753ea540c6b23116bc99337616f4f6086448bd4d
Drew Conly <atconly@gmail.com>
2016-07-04 20:19:14 -0600

    finish loop exercises

---
+++ loops.js
@@ -0,0 +1,41 @@
+var i = 0;
+while (i <= 500) {
+  console.log(i);
+  i += 100;
+}
+var i = 1;
+while (i <= 64) {
+  console.log(i);
+  i *= 2;
+}
+var i = 1;
+while (i <= 3) {
+  console.log(i);
+  console.log(i);
+  console.log(i);
+  i += 1;
+}
+var i = 0;
+while (i <= 10) {
+  console.log(i);
+  i += 2;
+}
+var i = 3;
+while (i <= 15) {
+  console.log(i);
+  i += 3;
+}
+var i = 9;
+while (i >= 0) {
+  console.log(i);
+  i -= 1;
+}
+var i = 0;
+var i2 = 3
+while (i2 > 0) {
+  console.log(i);
+  console.log(i + 1);
+  console.log(i + 2);
+  console.log(i + 3)
+  i2 -= 1;
+}
